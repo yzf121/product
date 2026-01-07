aeb5e07a87fcd0371591f967a09637af857f6e8e
yzf121 <1669590624@qq.com>
2026-01-07 10:27:29 +0800

    在增加文件上传功能之前的保存版本

--- app/ai-chat-gui/webapp/controller/Home.controller.js
+++ app/ai-chat-gui/webapp/controller/Home.controller.js
@@ -6,7 +6,7 @@ sap.ui.define([
     "use strict";
 
     return Controller.extend("com.ai.assistant.aichatapp.controller.Home", {
-        
+
         onInit: function () {
             // 首页控制器初始化
         },
@@ -19,29 +19,29 @@ sap.ui.define([
             var oTile = oEvent.getSource();
             var sAiType = oTile.data("aiType");
             var sHeader = oTile.getHeader();
-            
+
             // 获取路由并导航到聊天页面
             var oRouter = this.getOwnerComponent().getRouter();
             oRouter.navTo("chat", {
                 aiType: sAiType
             });
-            
+
             // 显示提示
             var oI18n = this.getView().getModel("i18n").getResourceBundle();
             MessageToast.show(oI18n.getText("enteringChat", [sHeader]));
         },
-        
+
         /**
          * 点击 Diagram Helper 卡片，跳转到流程图页面
          */
         onDiagramTilePress: function () {
             var oRouter = this.getOwnerComponent().getRouter();
             oRouter.navTo("diagram");
-            
+
             var oI18n = this.getView().getModel("i18n").getResourceBundle();
             MessageToast.show(oI18n.getText("enteringDiagram"));
         },
-        
+
         /**
          * 设置按钮点击事件
          */
