a817642935dce8fcfea2c31ee0a3d0f036dd6b81
Zefeng Yuan <zefeng.yuan@accenture.cn>
2026-01-11 12:56:59 +0000

    部署前1

---
+++ app/ai-chat-gui/dist/controller/App.controller.js
@@ -0,0 +1,2 @@
+sap.ui.define(["sap/ui/core/mvc/Controller"],function(n){"use strict";return n.extend("com.ai.assistant.aichatapp.controller.App",{onInit:function(){}})});
+//# sourceMappingURL=App.controller.js.map
